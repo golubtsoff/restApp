bbfb522b029b4d8df6e8a0cb88010dc55843f42e
Евгений Голубцов <evigol@mail.ru>
2019-02-11 15:37:53 +0700

    Initial commit

---
+++ src/main/java/MyRestService.java
@@ -0,0 +1,62 @@
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+import java.util.*;
+
+@Path("/persons")
+public class MyRestService {
+    private static List<Person> persons = new ArrayList<>();
+
+    @GET
+    @Path("/hello")
+    public Response hello() {
+        return Response.status(200).entity("hello").build();
+    }
+
+    @GET
+    @Path("/")
+    public List<Person> getPersons() {
+        return persons;
+    }
+
+    @GET
+    @Path("{pid}")
+    public Person getPerson(@PathParam("pid") int id) {
+        for(Person person : persons){
+            if (person.getId() == id)
+                return person;
+        }
+        return null;
+    }
+
+    @POST
+    @Path("/")
+    @Produces("application/xml")
+    public void addPerson(@FormParam("pid") int id,
+                          @FormParam("pname") String name){
+        persons.add(new Person(id, name));
+    }
+
+    @PUT
+    @Path("{pid}")
+    @Consumes("*/xml")
+    public void updatePerson(@PathParam("pid") int id,
+                             Person person){
+        for(Person p : persons){
+            if (p.getId() == id){
+                p.setName(person.getName());
+                return;
+            }
+        }
+    }
+
+    @DELETE
+    @Path("{pid}")
+    public void deletePerson(@PathParam("pid") int id){
+        for(int i = 0; i < persons.size(); i++){
+            if (persons.get(i).getId() == id){
+                persons.remove(i);
+                return;
+            }
+        }
+    }
+}
